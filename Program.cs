c0014758014cde336734ee72963430b28363c4ca
Ivanstarterprog <ivan.nurgazin@yandex.ru>
2024-03-08 01:43:25 +0700

    Добавлен класс меню с паттерном Синглтон

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
@@ -11,7 +12,14 @@ namespace StringsAndCollections
         private const string PhoneRegex = @"\((\d{3})\)\s?(\d{3})-(\d{2})-(\d{2})";
         private Dictionary<string, List<string>> _errorsDictionary;
         private string _workingDirectory { get; set; }
+        private string _dictionaryPath {  get; set; }
 
+        public StringErrorsFixer()
+        {
+            _workingDirectory = Directory.GetCurrentDirectory();
+            string dictionaryPath = _workingDirectory + "/dictionary.json";
+            ChangeDictionary(dictionaryPath);
+        }
 
         public StringErrorsFixer(string directory, string dictionary)
         {
@@ -19,7 +27,19 @@ namespace StringsAndCollections
             ChangeDictionary(dictionary);
         }
 
+        public static explicit operator string(StringErrorsFixer stringErrorsFixer)
+        {
+            return stringErrorsFixer.ToString();
+        }
 
+        public override string ToString()
+        {
+            StringBuilder errorFixerStringBuilder = new StringBuilder();
+            errorFixerStringBuilder.Append("В данный момент вы работаете в папке: " + _workingDirectory);
+            errorFixerStringBuilder.Append('\n');
+            errorFixerStringBuilder.Append("Выбран словарь по адресу: " + _dictionaryPath);
+            return errorFixerStringBuilder.ToString();
+        }
 
         public void ChangeDirectory(string newDirectory)
         {
@@ -51,26 +71,96 @@ namespace StringsAndCollections
             {
                 FixFile(file);
             }
-            Console.WriteLine("Текст файлов исправлен");
         }
 
         public void ChangeDictionary(string dictionaryPath)
         {
+            _dictionaryPath = dictionaryPath;
             using (StreamReader dictionaryStreamReader = new StreamReader(dictionaryPath))
             {
                 string fullDictionaryText = dictionaryStreamReader.ReadToEnd();
                 _errorsDictionary = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(fullDictionaryText);
             }
         }
+    }
 
+    class Menu
+    {
+        private static Menu instance;
+        private StringErrorsFixer _stringErrorsFixer = new StringErrorsFixer();
+        public static Menu Instance
+        {
 
+            get
+            {
+                if (instance == null) instance = new Menu();
+                return instance;
+            }
+        }
+
+        public void MenuRender() 
+        {
+            Console.WriteLine(_stringErrorsFixer);
+            MenuOperationChoice();
+            Console.Clear();
+        }
+
+        public void MenuFixerStart()
+        {
+            _stringErrorsFixer.StartFixingFiles();
+            Console.WriteLine("Текст файлов исправлен");
+        }
+
+        public void MenuChangeDirectory()
+        {
+            Console.Write("Введите новую директорию: ");
+            string newDirectory = Console.ReadLine();
+            _stringErrorsFixer.ChangeDirectory(newDirectory);
+        }
+
+        public void MenuChangeDictionary()
+        {
+            Console.Write("Введите новый словарь: ");
+            string newDictionary = Console.ReadLine();
+            _stringErrorsFixer.ChangeDictionary(newDictionary);
+        }
+        public void MenuOperationChoice()
+        {
+            Console.WriteLine("Выберите следующую операцию: ");
+            Console.WriteLine("1. Запустить исправитель");
+            Console.WriteLine("2. Изменить директорию");
+            Console.WriteLine("3. Указать адрес до словаря");
+            Console.Write("Введите номер операции: ");
+            int operationChoice = Convert.ToInt16(Console.ReadLine());
+            switch(operationChoice)
+            {
+                case 1:
+                    MenuFixerStart();
+                    break;
+                case 2:
+                    MenuChangeDirectory();
+                    break;
+                case 3:
+                    MenuChangeDictionary();
+                    break;
+                default:
+                    Console.WriteLine("Неопознанная команда! Попробуйте заного");
+                    break;
+            }
+            Console.WriteLine("Нажмите любую кнопку чтобы продолжить...");
+            Console.ReadKey();
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Menu mainMenu = new Menu();
+            while (true)
+            {
+                mainMenu.MenuRender();
+            }
         }
     }
 }
